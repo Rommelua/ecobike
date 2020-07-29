5774d6a9cb1ba9e8d38b19f023a463220e55a870
Leonid <rommel@ukr.net>
2020-07-29 23:55:01 +0300

    findBikesByParameter() completed

--- src/main/java/com/ecobike/DataHolder.java
+++ src/main/java/com/ecobike/DataHolder.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DataHolder {
     private final static DataHolder INSTANCE = new DataHolder();
@@ -36,6 +37,33 @@ public class DataHolder {
 
     public List<Bike> getUnmodifiableBikeList() {
         return Collections.unmodifiableList(bikes);
+    }
+
+    public List<Bike> findBikesByParameter(SearchParameterContainer parCont) {
+        Stream<Bike> bikeStream = bikes.parallelStream()
+                .filter(bike -> bike.getBikeType() == parCont.getBikeType()
+                    && bike.getBrand().equalsIgnoreCase(parCont.getBrand())
+                    && bike.getWeight() >= parCont.getMinWeight()
+                    && (parCont.getMaxWeight() == 0 || bike.getWeight() <= parCont.getMaxWeight())
+                    && (!parCont.isLightsOptionEntered() || parCont.isLightsPresent() == bike.isLightsPresent())
+                    && (parCont.getColor().isEmpty() || parCont.getColor().equalsIgnoreCase(bike.getColor()))
+                    && bike.getPrice() >= parCont.getMinPrice()
+                    && (parCont.getMaxPrice() == 0 || bike.getPrice() <= parCont.getMaxPrice()));
+
+        if (parCont.getBikeType() == BikeType.FOLDING_BIKE) {
+            return bikeStream.map(bike -> (FoldingBike) bike)
+                    .filter(bike -> bike.getWheelSize() >= parCont.getMinWheelSize()
+                        && (parCont.getMaxWheelSize() == 0 || bike.getWheelSize() <= parCont.getMaxWheelSize())
+                        && bike.getNumberOfGears() >= parCont.getMinNumberOfGears()
+                        && (parCont.getMaxNumberOfGears() == 0 || bike.getNumberOfGears() <= parCont.getMaxNumberOfGears()))
+                    .collect(Collectors.toList());
+        }
+        return bikeStream.map(bike -> (AbstractElectroBike) bike)
+                .filter(bike -> bike.getMaxSpeed() >= parCont.getMinMaxBikeSpeed()
+                    && (parCont.getMaxMaxBikeSpeed() == 0 || bike.getMaxSpeed() <= parCont.getMaxMaxBikeSpeed())
+                    && bike.getBatteryCapacity() >= parCont.getMinBatteryCapacity()
+                    && (parCont.getMaxBatteryCapacity() == 0 || bike.getBatteryCapacity() <= parCont.getMaxBatteryCapacity()))
+                .collect(Collectors.toList());
     }
 
     private Bike parseBike(String line) {
--- src/main/java/com/ecobike/command/FindCommand.java
+++ src/main/java/com/ecobike/command/FindCommand.java
@@ -1,12 +1,13 @@
 package com.ecobike.command;
 
+import com.ecobike.SearchParameterContainer;
 import com.ecobike.model.BikeType;
 
 public class FindCommand implements Command {
 
     @Override
     public void execute() {
-        BikeType bikeType;
+        SearchParameterContainer paramContainer = new SearchParameterContainer();
         while (true) {
             COMMUNICATOR.writeMessage("Select bike type you wont to find");
             COMMUNICATOR.writeMessage("\t 1 - " + BikeType.FOLDING_BIKE);
@@ -14,7 +15,7 @@ public class FindCommand implements Command {
             COMMUNICATOR.writeMessage("\t 3 - " + BikeType.SPEEDELEC);
             int bikeTypeNumber;
             if ((bikeTypeNumber = COMMUNICATOR.readInt()) >= 1 && bikeTypeNumber <= 3) {
-                bikeType = BikeType.values()[bikeTypeNumber - 1];
+                paramContainer.setBikeType(BikeType.values()[bikeTypeNumber - 1]);
                 break;
             }
             COMMUNICATOR.writeMessage("=== Wrong entry ===");
@@ -22,76 +23,71 @@ public class FindCommand implements Command {
         }
 
         COMMUNICATOR.writeMessage("Enter bike brand:");
-        String brand = "";
+        String brand;
         while ((brand = COMMUNICATOR.readString()).isEmpty()) {
             COMMUNICATOR.writeMessage("Can't skip. Enter bike brand:");
         }
+        paramContainer.setBrand(brand);
 
         COMMUNICATOR.writeMessage("You may choose next parameters (for skipping parameter press \"Enter\"):");
         COMMUNICATOR.writeMessage("Enter min weight:");
-        int minWeight = COMMUNICATOR.readInt();
+        paramContainer.setMinWeight(COMMUNICATOR.readInt());
 
         COMMUNICATOR.writeMessage("Enter max weight:");
-        int maxWeight = COMMUNICATOR.readInt();
+        paramContainer.setMaxWeight(COMMUNICATOR.readInt());
 
         COMMUNICATOR.writeMessage("Enter lights presence (Type 1 for TRUE or 2 for FALSE):");
-        boolean isLightsOptionEntered = false;
-        boolean isLightsPresent;
         while (true) {
             String entry = COMMUNICATOR.readString();
             if (entry.isEmpty()) {
                 break;
             }
             if (entry.equals("1")) {
-                isLightsPresent = true;
-                isLightsOptionEntered = true;
+                paramContainer.setLightsPresent(true);
+                paramContainer.setLightsOptionEntered(true);
                 break;
             }
             if (entry.equals("2")) {
-                isLightsPresent = false;
-                isLightsOptionEntered = true;
+                paramContainer.setLightsPresent(false);
+                paramContainer.setLightsOptionEntered(true);
                 break;
             }
             System.out.println("Wrong entry. Type 1 for TRUE, 2 for FALSE or \"Enter\" for skipping");
         }
 
         COMMUNICATOR.writeMessage("Enter color:");
-        String color = COMMUNICATOR.readString();
+        paramContainer.setColor(COMMUNICATOR.readString());
 
         COMMUNICATOR.writeMessage("Enter min price:");
-        int minPrice = COMMUNICATOR.readInt();
+        paramContainer.setMinPrice(COMMUNICATOR.readInt());
 
         COMMUNICATOR.writeMessage("Enter max price:");
-        int maxPrice = COMMUNICATOR.readInt();
+        paramContainer.setMaxPrice(COMMUNICATOR.readInt());
 
-        int minWheelSize;
-        int maxWheelSize;
-        int minNumberOfGears;
-        int maxNumberOfGears;
-        if (bikeType == BikeType.FOLDING_BIKE) {
+        if (paramContainer.getBikeType() == BikeType.FOLDING_BIKE) {
             COMMUNICATOR.writeMessage("Enter min wheel size:");
-            minWheelSize = COMMUNICATOR.readInt();
+            paramContainer.setMinWheelSize(COMMUNICATOR.readInt());
             COMMUNICATOR.writeMessage("Enter max wheel size:");
-            maxWheelSize = COMMUNICATOR.readInt();
+            paramContainer.setMaxWheelSize(COMMUNICATOR.readInt());
             COMMUNICATOR.writeMessage("Enter min number of gears:");
-            minNumberOfGears = COMMUNICATOR.readInt();
+            paramContainer.setMinNumberOfGears(COMMUNICATOR.readInt());
             COMMUNICATOR.writeMessage("Enter max number of gears:");
-            maxNumberOfGears = COMMUNICATOR.readInt();
+            paramContainer.setMaxNumberOfGears(COMMUNICATOR.readInt());
         }
 
-        int minMaxBikeSpeed;
-        int maxMaxBikeSpeed;
-        int minBatteryCapacity;
-        int maxBatteryCapacity;
-        if (bikeType == BikeType.E_BIKE || bikeType == BikeType.SPEEDELEC) {
+        if (paramContainer.getBikeType() == BikeType.E_BIKE
+                || paramContainer.getBikeType() == BikeType.SPEEDELEC) {
             COMMUNICATOR.writeMessage("Enter minimum max bike speed:");
-            minMaxBikeSpeed = COMMUNICATOR.readInt();
+            paramContainer.setMinMaxBikeSpeed(COMMUNICATOR.readInt());
             COMMUNICATOR.writeMessage("Enter maximum max bike speed:");
-            maxMaxBikeSpeed = COMMUNICATOR.readInt();
+            paramContainer.setMaxMaxBikeSpeed(COMMUNICATOR.readInt());
             COMMUNICATOR.writeMessage("Enter min battery capacity:");
-            minBatteryCapacity = COMMUNICATOR.readInt();
+            paramContainer.setMinBatteryCapacity(COMMUNICATOR.readInt());
             COMMUNICATOR.writeMessage("Enter max battery capacity:");
-            maxBatteryCapacity = COMMUNICATOR.readInt();
+            paramContainer.setMaxBatteryCapacity(COMMUNICATOR.readInt());
         }
+
+        DATA_HOLDER.findBikesByParameter(paramContainer)
+                .forEach(bike -> COMMUNICATOR.writeMessage(bike.toString()));
     }
 }
